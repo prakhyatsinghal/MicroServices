b1922d49f08101e4114f4e5d09e2d8c1af108510
Prakhyat Singhal <singhalprakhyat2001@gmail.com>
2022-12-14 20:19:20 +0530

    Initialised The Project

---
+++ src/main/java/com/load/user/service/entities/User.java
@@ -0,0 +1,29 @@
+package com.load.user.service.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "micro_users")public class User {
+
+    @Id
+    @Column(name = "ID")
+    private String userId ;
+
+    @Column(name = "NAME", length = 20)
+    private String name;
+
+    @Column(name = "EMAIL")
+    private String email;
+
+    @Column(name = "ABOUT")
+    private String about;
+
+
+
+}
